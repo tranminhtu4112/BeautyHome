fb90763b335e0f3dcf0cb972df77ea1ae4b3e324
lam <lam719123@gmail.com>
2021-07-04 19:46:10 +0700

    l

---
+++ BeautyHome/Controllers/Admin_AddproductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeautyHome.Controllers
+{
+    public class Admin_AddproductController : Controller
+    {
+        // GET: Admin_Addproduct
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BeautyHome/Controllers/Admin_HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeautyHome.Controllers
+{
+    public class Admin_HomeController : Controller
+    {
+        // GET: Admin_Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
