782b0b42b20f02d0dd00232f024d9bc31c1c1f75
lam <lam719123@gmail.com>
2021-07-11 16:57:54 +0700

    1

--- BeautyHome/Controllers/ProductController.cs
+++ BeautyHome/Controllers/ProductController.cs
@@ -28,6 +28,8 @@ namespace BeautyHome.Controllers
                 "from product, furniture, type_product " +
                 "where type_product.type_product_id = product.type_product_id " +
                 "and furniture.furniture_id = type_product.furniture_id and furniture.furniture_id = " + furId;
+
+
             List<ProductView> listpr = new List<ProductView>();
             SqlCommand cmd = new SqlCommand();
             connection.Open();
@@ -56,7 +58,46 @@ namespace BeautyHome.Controllers
             objtypeProductView.listProductViews = listpr;
             return View(objtypeProductView);
         }
+        public ActionResult Type(long typeprId)
+        {
+            TypeProductView objtypeProductView = new TypeProductView();
+            var listtype = db.type_product.ToList();
+            var listfur = db.furnitures.ToList();
+            objtypeProductView.listtype = listtype;
+            objtypeProductView.listfur = listfur;
+            string sql = "select * " +
+                "from product, furniture, type_product " +
+                "where type_product.type_product_id = product.type_product_id " +
+                "and furniture.furniture_id = type_product.furniture_id and type_product.type_product_id = " + typeprId;
 
+            List<ProductView> listpr = new List<ProductView>();
+            SqlCommand cmd = new SqlCommand();
+            connection.Open();
+            cmd.Connection = connection;
+            cmd.CommandText = sql;
+            using (DbDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        ProductView productView = new ProductView();
+
+                        productView.productId = Convert.ToInt64(reader.GetValue(0));
+                        productView.name = Convert.ToString(reader.GetValue(2));
+                        productView.descriptionDetails = Convert.ToString(reader.GetValue(3));
+                        productView.description = Convert.ToString(reader.GetValue(4));
+                        productView.evaluate = Convert.ToDouble(reader.GetValue(5));
+                        productView.amount = Convert.ToDouble(reader.GetValue(6));
+                        productView.price = Convert.ToDouble(reader.GetValue(7));
+                        productView.color = Convert.ToString(reader.GetValue(8));
+                        listpr.Add(productView);
+                    }
+                }
+            }
+            objtypeProductView.listProductViews = listpr;
+            return View(objtypeProductView);
+        }
 
 
     }
